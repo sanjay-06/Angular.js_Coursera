897c683dd33a17f1d31c8bdd7d05878e6ca98ef5
sanjay <san692001@gmail.com>
2022-01-11 12:20:11 +0530

    assignment 3

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit ,Input } from '@angular/core';
-import { Dish } from '../shared/dish';
-import { DishService } from '../services/dish.service';
+import { Comment } from './../shared/coment'
+import { Location } from '@angular/common'
+import { Component, OnInit, ViewChild } from '@angular/core'
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
+import { ActivatedRoute } from '@angular/router'
+import { switchMap } from 'rxjs/operators'
 
-import { Params, ActivatedRoute } from '@angular/router';
-import { Location } from '@angular/common';
-import { switchMap } from 'rxjs/operators';
+import { DishService } from './../services/dish.service'
+import { Dish } from './../shared/dish'
 
 @Component({
   selector: 'app-dishdetail',
@@ -12,30 +14,99 @@ import { switchMap } from 'rxjs/operators';
   styleUrls: ['./dishdetail.component.scss']
 })
 export class DishdetailComponent implements OnInit {
+  dish: Dish
+  dishIds: string[]
+  prev: string
+  next: string
+  commentForm: FormGroup
+  comment: Comment
 
-    dish: Dish;
-    dishIds: string[];
-    prev: string;
-    next: string;
-  
-    constructor(private dishservice: DishService,
-      private route: ActivatedRoute,
-      private location: Location) { }
-  
-    ngOnInit() {
-      this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
-      this.route.params.pipe(switchMap((params: Params) => this.dishservice.getDish(params['id'])))
-      .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); });
-    }
+  @ViewChild('fform') commentFormDirective
 
-    setPrevNext(dishId: string) {
-      const index = this.dishIds.indexOf(dishId);
-      this.prev = this.dishIds[(this.dishIds.length + index - 1) % this.dishIds.length];
-      this.next = this.dishIds[(this.dishIds.length + index + 1) % this.dishIds.length];
-    }
-  
-    goBack(): void {
-      this.location.back();
-    }
-  
+  formErrors = {
+    author: '',
+    rating: '',
+    comment: '',
   }
+
+  validationMessages = {
+    author: {
+      required: 'Author Name is required.',
+      minlength: 'Author Name must be at least 2 characters long.',
+    },
+    comment: {
+      required: 'Comment is required.',
+    },
+  }
+
+  constructor(
+    private dishService: DishService,
+    private route: ActivatedRoute,
+    private location: Location,
+    private fb: FormBuilder
+  ) {
+    this.createForm()
+  }
+
+  ngOnInit(): void {
+    this.dishService.getDishIds().subscribe(dishIds => (this.dishIds = dishIds))
+    this.route.params
+      .pipe(switchMap(params => this.dishService.getDish(params['id'])))
+      .subscribe(dish => {
+        this.dish = dish
+        this.setPrevNext(dish.id)
+      })
+  }
+
+  setPrevNext(dishId: string | undefined) {
+    const index = this.dishIds.indexOf(dishId)
+    const length = this.dishIds.length
+    this.prev = this.dishIds[(length + index - 1) % length]
+    this.next = this.dishIds[(length + index + 1) % length]
+  }
+
+  goBack(): void {
+    this.location.back()
+  }
+
+  createForm() {
+    this.commentForm = this.fb.group({
+      author: ['', [Validators.required, Validators.minLength(2)]],
+      comment: ['', [Validators.required]],
+      rating: 5,
+    })
+
+    this.commentForm.valueChanges.subscribe(data => this.onValueChanged(data))
+  }
+
+  onSubmit() {
+    this.comment = this.commentForm.value
+    this.comment.date = new Date().toISOString()
+    this.dish.comments.push(this.comment)
+    this.commentForm.reset({
+      author: '',
+      rating: 5,
+      comment: '',
+    })
+    this.commentFormDirective.resetForm()
+  }
+
+  onValueChanged(data: any) {
+    if (!this.commentForm) return
+    const form = this.commentForm
+    for (const field in this.formErrors) {
+      if (this.formErrors.hasOwnProperty(field)) {
+        this.formErrors[field] = ''
+        const control = form.get(field)
+        if (control && control.dirty && !control.valid) {
+          const messages = this.validationMessages[field]
+          for (const key in control.errors) {
+            if (control.errors.hasOwnProperty(key)) {
+              this.formErrors[field] += messages[key] + ' '
+            }
+          }
+        }
+      }
+    }
+  }
+}
